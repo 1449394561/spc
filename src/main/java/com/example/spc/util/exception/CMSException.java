8804796e2308ac3d77c172df4e1ac8b8c041cbaf
lisngwenlong <1449394561@qq.com>
2022-12-02 16:31:50 +0800

    Initial commit

---
+++ src/main/java/com/example/spc/util/exception/CMSException.java
@@ -0,0 +1,26 @@
+package com.example.spc.util.exception;
+
+
+import com.example.spc.util.ResultCodeEnum;
+import lombok.Data;
+
+//统一异常处理
+@Data
+public class CMSException extends RuntimeException {
+    private Integer code;
+
+    public CMSException(Integer code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public CMSException(ResultCodeEnum resultCodeEnum) {
+        super(resultCodeEnum.getMessage());
+        this.code = resultCodeEnum.getCode();
+    }
+
+    @Override
+    public String toString() {
+        return "CMSException{" + "code=" + code + ", message=" + this.getMessage() + '}';
+    }
+}
