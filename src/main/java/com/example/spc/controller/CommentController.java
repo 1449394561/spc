033d26139aa61cc1ff558c826ae98bcadda5aeae
lisngwenlong <1449394561@qq.com>
2022-12-02 09:48:21 +0800

    Initial commit

---
+++ src/main/java/com/example/spc/controller/CommentController.java
@@ -0,0 +1,40 @@
+package com.example.spc.controller;
+
+
+import com.example.spc.entity.Wcha;
+import com.example.spc.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: HardyYao
+ * @Date: 2021/6/19
+ */
+@RequestMapping("/redis")
+@RestController
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @RequestMapping(value = "/findCommentById")
+    public Wcha findCommentById(Integer id){
+        Wcha comment = commentService.findCommentById(id);
+        return comment;
+    }
+
+    @RequestMapping(value = "/updateComment")
+    public Wcha updateComment(Wcha comment){
+        Wcha oldComment = commentService.findCommentById(comment.getId());
+
+        Wcha comment1 = commentService.updateComment(comment);
+        return comment1;
+    }
+
+    @RequestMapping(value = "/deleteComment")
+    public void deleteComment(Integer id){
+        commentService.deleteComment(id);
+    }
+
+}
---
+++ src/main/java/com/example/spc/service/CommentService.java
@@ -0,0 +1,63 @@
+package com.example.spc.service;
+
+
+
+import com.example.spc.entity.Wcha;
+import com.example.spc.mapper.WchaMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * @Author: HardyYao
+ * @Date: 2021/6/19
+ */
+@Service
+public class CommentService {
+
+    @Autowired
+    private WchaMapper commentRepository;
+
+    /**
+     * 根据评论id查询评论
+     * @Cacheable：将该方法的查询结果comment存放在SpringBoot默认缓存中
+     * cacheNames：起一个缓存命名空间，对应缓存唯一标识
+     * @param id
+     * @return
+     */
+    @Cacheable(cacheNames = "comment", unless = "#result==null")
+    public Wcha findCommentById(Integer id){
+        Wcha wcha = commentRepository.selectById(id);
+        Optional<Wcha> comment = Optional.ofNullable(wcha);
+        if(comment.isPresent()){
+            Wcha comment1 = comment.get();
+            return comment1;
+        }
+        return null;
+    }
+
+    /**
+     * 更新评论
+     * @param comment
+     * @return
+     */
+    @CachePut(cacheNames = "comment",key = "#result.id")
+    public Wcha updateComment(Wcha comment) {
+        commentRepository.updateById(comment);
+        return comment;
+    }
+
+    /**
+     * 删除评论
+     * @param comment_id
+     */
+    @CacheEvict(cacheNames = "comment")
+    public void deleteComment(int comment_id) {
+        commentRepository.deleteById(comment_id);
+    }
+
+}
